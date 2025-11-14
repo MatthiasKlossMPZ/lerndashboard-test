c1f0219f62392e755560aedae87bc2c3695f96de
Matthias Kloß <m.kloss@schule-mv.de>
2025-11-14 18:13:36 +0100

    v34

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const VERSION = '1.5.3.32';  // Erhöhe bei jedem Update!
+const VERSION = '1.5.3.34';  // Erhöhe bei jedem Update!
 const CACHE_NAME = `lerndashboard-v${VERSION.replace(/\./g, '')}`;
 const REPO_PATH = 'https://matthiasklossmpz.github.io/lerndashboard-test/';
 
