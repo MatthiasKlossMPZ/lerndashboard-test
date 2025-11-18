48b607eff5ed0b7fe7ceb816c01a4a9a7c151375
MatthiasKlossMPZ <m.kloss@schule-mv.de>
2025-11-18 21:29:24 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const VERSION = '1.6.20';                     // Nur hier erhöhen bei Änderungen!
+const VERSION = '1.6.21';                     // Nur hier erhöhen bei Änderungen!
 const CACHE_NAME = `lerndashboard-v${VERSION.replace(/\./g, '')}`;
 
 // WICHTIG: Dynamischer Pfad mit garantiertem trailing Slash
